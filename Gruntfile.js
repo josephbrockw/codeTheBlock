55afd1082ab029197b7f8e4ab5222414a240da71
Joe Wilkinson <joe@codekoalas.com>
2016-07-11 16:52:35 -0500

    Added CK grunt file

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,58 +1,116 @@
-module.exports = function (grunt) {
+module.exports = function(grunt) {
+  require('time-grunt')(grunt);
   require('load-grunt-tasks')(grunt);
+  grunt.loadNpmTasks('grunt-postcss'); // not sure why this is needed
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
-    concat: {
-      dist: {
-        src: ['themes/zurb_foundation/STARTER/js/*.js'],
-        dest: 'themes/zurb_foundation/js/script.js'
-      }
-    },
-
     sass: {
       dist: {
         options: {
-          style: 'compressed',
-          //ncludePaths: ['scss'],
+          style: 'expanded',
         },
         files: {
-          'css/main.min.css': 'css/scss/init.scss',
-          'themes/zurb_foundation/css/zurb_foundation.min.css': 'themes/zurb_foundation/scss/zurb_foundation.scss',
-          //'css/custom.min.css': 'themes/zurb_foundation/STARTER/scss/**/*.scss'
-          // 'scss/foundation.scss': 'css/foundation.min.css',
-          // 'scss/zurb_foundation.scss': 'css/zurb_foundation.min.css'
+          'css/2016.css': 'scss/2016.scss'
         }
       }
     },
-
-    copy: {
+    babel: {
+      options: {
+        presets: ['es2015'],
+        sourceMap: true
+      },
       dist: {
-        files: [
-          {expand: true, cwd: 'bower_components/foundation/css/', src: ['*'], dest: 'css/', filter: 'isFile'},
-          {expand: true, cwd: 'bower_components/foundation/js/', src: ['**'], dest: 'js/'},
-          {expand: true, cwd: 'bower_components/foundation/scss/', src: ['**'], dest: 'scss/'}
-        ]
+        'files': { 'js/2016.min.js': 'js/2016.js' }
+      }
+    },
+    eslint: {
+      options: {
+        configFile: '.eslintrc'
+      },
+      target: ['js/_2016.js']
+    },
+    concat: {
+      options: {
+        sourceMap: true,
+        sourceMapStyle: 'link'
+      },
+      js: {
+        src: [
+          'js/_2016.js',
+        ],
+        dest: 'js/2016.js'
+      }
+    },
+    postcss: {
+      //lint: {
+        options: {
+          syntax: require('postcss-scss'),
+          processors: [
+            require("stylelint")({
+              "extends": [
+                "stylelint-config-standard",
+              ],
+              "plugins": [
+                "stylelint-statement-max-nesting-depth",
+              ],
+              "rules": {
+                "statement-max-nesting-depth": [3, { countAtRules: false }],
+                "string-quotes": "single",
+                "selector-no-id": true,
+                "color-hex-case": "lower",
+                "number-leading-zero": "never",
+                "property-no-vendor-prefix": true,
+                "declaration-bang-space-after": "never",
+                "declaration-bang-space-before": "always",
+                "declaration-colon-space-after": "always",
+                "declaration-no-important": true,
+                "selector-combinator-space-after": "always",
+                "selector-combinator-space-before": "always",
+                "selector-pseudo-element-colon-notation": "single",
+                "rule-nested-empty-line-before": "always",
+                "declaration-block-properties-order": "alphabetical"
+              }
+            }),
+            require("postcss-reporter")({ clearMessages: true })
+          ]
+        },
+        dist: {
+          src: 'scss/**/*.scss'
+        }
+      //}
+    },
+    autoprefixer: {
+      options: {
+        browsers: ['> 1%', 'last 2 versions', 'ie 9'],
+        remove: false
+      },
+      dist: {
+        src: 'css/2016.css',
       }
     },
 
     watch: {
-      grunt: {files: ['Gruntfile.js']},
+      grunt: { files: ['Gruntfile.js'] },
 
       sass: {
-        files: 'themes/zurb_foundation/STARTER/**/*.scss',
-        tasks: ['sass']
-      }
+        files: 'scss/**/*.scss',
+        tasks: ['postcss', 'sass', 'autoprefixer'],
+        options: {
+          livereload: true
+        }
+      },
+      babel: {
+        files: 'js/_2016.js',
+        tasks: ['concat', 'babel']
+      },
     }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-sass');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.registerTask('new-build', ['newer:jshint:all','newer:uglify:dist','newer:sass:dist','newer:autoprefixer:dist']);
+  grunt.registerTask('new', ['new-build', 'watch']);
 
-  grunt.registerTask('build', ['copy', 'sass']);
-  
+  grunt.registerTask('build', [/*'eslint',*/ 'concat', 'babel','postcss','sass','autoprefixer']);
   grunt.registerTask('default', ['build', 'watch']);
 };
